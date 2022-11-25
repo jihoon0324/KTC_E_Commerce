fef7fe35e5fd07e801c7f51ef07e96b2df579f7d
KaiLee85 <87049080+KaiLee85@users.noreply.github.com>
2022-11-24 21:01:41 -0700

    install nodemon

--- backend/Server.js
+++ backend/Server.js
@@ -1,12 +1,18 @@
-import ProductRouters from "./routers/ProductRouters";
-
 const express = require("express");
+
+//import routes
+// import ProductRouters from "./routers/ProductRouters";
 
 require("dotenv").config();
 const app = express();
 const PORT = process.env.port || 5000;
 
-const connection = require("./database/db");
-connection();
+// const connection = require("./database/db");
+// connection();
 
-app.use("/api/products", ProductRouters);
+app.use(express.json());
+
+// app.use("/api/products", ProductRouters);
+app.listen(PORT, (res, req) => {
+  console.log("connect server");
+});
