2ba7326414395a96f94f472514cc0573d9720a74
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-11-26 00:11:22 -0700

    Update product router and controller

--- backend/routers/ProductRouters.js
+++ backend/routers/ProductRouters.js
@@ -3,10 +3,16 @@ const express = require("express");
 const productRouter = express.Router();
 
 const {
+  getAllProducts,
+  getProductsByCategory,
+  addProduct,
   updateProduct,
   deleteProduct,
 } = require("../controller/ProductController");
 
+productRouter.get("/", getAllProducts);
+productRouter.get("/:categoryId", getProductsByCategory);
+productRouter.post("/add", addProduct);
 productRouter.route("/:id").put(updateProduct).delete(deleteProduct);
 
 module.exports = productRouter;
