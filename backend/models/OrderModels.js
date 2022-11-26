cc9c058d8104f852a308edd8ad4ade23a744f8c4
Lauren <lauren.sung23@gmail.com>
2022-11-26 09:45:35 -0700

    update order model

--- backend/models/OrderModels.js
+++ backend/models/OrderModels.js
@@ -22,11 +22,17 @@ const OrderSchema = new mongoose.Schema({
     ref: "User",
     required: true,
   },
-  productArray: [
+  orderItems: [
     {
-      type: mongoose.Types.ObjectId,
+      name: {type: String, required: true},
+      qty: {type: Number, required: true},
+      image: {type:String, required:true},
+      price: {type:Number, required:true},
+      product:{
+      type: mongoose.Schema.Types.ObjectId,
       ref: "Product",
       required: true,
+      },
     },
   ],
 });
