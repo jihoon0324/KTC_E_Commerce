fa5a537ca63e11ce831da48f3d9b5f87cb9535e9
Jihoon Oh <jihoon.oh@edu.sait.ca>
2022-12-07 13:43:02 -0700

    add header

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,7 +1,43 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
+import { FaShoppingCart } from 'react-icons/fa';
 const Header = () => {
-  return <div style={{ border: '1px  solid red' }}>Header</div>;
+  const [nameTest, setNameTest] = useState('KTC');
+  return (
+    <>
+      <Navbar style={{ border: '1px  solid red' }}>
+        <Container>
+          <Nav>
+            <Link to='/'>
+              <Navbar.Brand>KTC Shop</Navbar.Brand>
+            </Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href='/'>Blouse</Nav.Link>
+            <Nav.Link href='/'>Dress</Nav.Link>
+            <Nav.Link href='/'>Pants</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href='/'>Login</Nav.Link>
+            <NavDropdown title={nameTest} id='basic-nav-dropdown'>
+              <NavDropdown.Item href=''>My Page</NavDropdown.Item>
+              <NavDropdown.Item href='/orderhistory'>
+                Ore History
+              </NavDropdown.Item>
+              <NavDropdown.Item href=''>Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href=''>Logout 은 어디있나 ?</NavDropdown.Item>
+            </NavDropdown>
+            <div>
+              <FaShoppingCart size={20} />
+            </div>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 
 export default Header;
