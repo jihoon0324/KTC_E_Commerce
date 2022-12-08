d119ecd5ff6ee74d6af5fb48628319bc5f6c2783
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-12-08 16:51:08 -0700

    Catch errors in product api and slice

--- frontend/src/utils/product/productHandleApi.js
+++ frontend/src/utils/product/productHandleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const getProduct = async () => {
+export const getProduct = async (id) => {
   try {
     const res = await axios.get(
       `http://localhost:5000/api/products//${id}`
