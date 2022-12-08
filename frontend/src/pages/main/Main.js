22165eb6e57f54cfd2a68d69d02bad5393ab99e5
Lauren <lauren.sung23@gmail.com>
2022-12-08 01:13:47 -0700

    fix css and update main

--- frontend/src/pages/main/Main.js
+++ frontend/src/pages/main/Main.js
@@ -7,50 +7,54 @@ import imageThird from '../../src_assets/main-2.jpg';
 
 const Main = () => {
   return (
- 
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className='d-block'
-          src={imageFirst}
-          alt='First slide'
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className='d-block'
-          src={imageSecond}
-          alt='Second slide'
-        />
+    <>
+      <main className='mainPage'>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className='d-block'
+              src={imageFirst}
+              alt='First slide'
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className='d-block'
+              src={imageSecond}
+              alt='Second slide'
+            />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className='d-block'
-          src={imageThird}
-          alt='Third slide'
-        />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className='d-block'
+              src={imageThird}
+              alt='Third slide'
+            />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
 
-  )
-}
+        <div className='show_Cover'>
+          <div className='show_Name'>Show All Products</div>
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default Main;
-
-
