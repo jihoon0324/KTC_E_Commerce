69a71a39333d0db1db3be28e9d9d44bed39b7afd
Lauren <lauren.sung23@gmail.com>
2022-12-07 22:27:19 -0700

    update page slider

--- frontend/src/pages/main/Main.js
+++ frontend/src/pages/main/Main.js
@@ -1,17 +1,52 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import './Main.css';
+import imageFirst from '../../src_assets/main-1.jpg';
+import imageSecond from '../../src_assets/main-4.jpg';
+import imageThird from '../../src_assets/main-2.jpg';
 
 const Main = () => {
   return (
-    <div
-      style={{
-        border: '1px  solid red',
-        backgroundColor: 'green',
+ 
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className='d-block'
+          src={imageFirst}
+          alt='First slide'
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className='d-block'
+          src={imageSecond}
+          alt='Second slide'
+        />
 
-        height: '500px',
-      }}
-    >
-      Main
-    </div>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className='d-block'
+          src={imageThird}
+          alt='Third slide'
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
 
